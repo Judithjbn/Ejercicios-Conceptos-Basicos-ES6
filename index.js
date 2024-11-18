8a8c47b7a5d83b1a96d9409f2cf04ec9e8604f49
Judith <jbarrilnavarro@gmail.com>
2024-11-18 12:00:39 +0100

    Primer y segundo ejercicio

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //  Ejercicios Conceptos Básicos ES6+
+// Crea una arrow function que tenga dos parametros a y b y que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre por consola la suma de los dos parametros. 
+
+const suma = (a= 10, b=5) => {
+    console.log(a + b);
+}
+suma();
+suma(3);
+suma(3,5);
+
+// En base al siguiente javascript, crea variables en base a las propiedades del objeto usando object destructuring e imprimelas por consola. Cuidado, no hace falta hacer destructuring del array, solo del objeto.
+const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+const {title, gender, year} = game;

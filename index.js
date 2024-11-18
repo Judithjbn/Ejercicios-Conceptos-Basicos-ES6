34d6588c6109ae7cb863c0bee2ae356ecd955224
Judith <50967294+Judithjbn@users.noreply.github.com>
2024-11-18 21:49:07 +0000

    ejercicios 1,2,3

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
 //  Ejercicios Conceptos Básicos ES6+
+
+// Ejercicio 1
 // Crea una arrow function que tenga dos parametros a y b y que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre por consola la suma de los dos parametros. 
 
-const suma = (a= 10, b=5) => {
+const suma = (a = 10, b = 5) => {
     console.log(a + b);
 }
 suma();
 suma(3);
-suma(3,5);
+suma(3, 5);
 
-// En base al siguiente javascript, crea variables en base a las propiedades del objeto usando object destructuring e imprimelas por consola. Cuidado, no hace falta hacer destructuring del array, solo del objeto.
-const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
-const {title, gender, year} = game;
+// Ejercicio 2
+// 2. En base al siguiente javascript, crea variables en base a las propiedades del objeto usando object destructuring e imprimelas por consola. Cuidado, no hace falta hacer destructuring del array, solo del objeto.
+const game = { title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020 }
+const { title, gender, year } = game;
+
+// En base al siguiente javascript, usa destructuring para crear 3 variables llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente imprimelo por consola.
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1, fruit2, fruit3);
+
+// En base al siguiente javascript, usa destructuring para crear 2 variables igualandolo a la función e imprimiendolo por consola.
+const animalFunction = () => {
+    return { name: 'Bengal Tiger', race: 'Tiger' }
+};
+const { name, race } = animalFunction();
+
+// En base al siguiente javascript, usa destructuring para crear las variables name y itv con sus respectivos valores. Posteriormente crea 3 variables usando igualmente el destructuring para cada uno de los años y comprueba que todo esta bien imprimiendolo.
+const car = { nameCar: 'Mazda 6', itv: [2015, 2011, 2020] }
+const { nameCar, itv } = car;
+console.log(nameCar);
+console.log(itv);
+const [year1, year2, year3] = itv;
+console.log(year1, year2, year3);
+
+// Ejercicio 3
+// Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43]
+const pointsListCopy = [...pointsList];
+
+// Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = { name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor' };
+const toyCopy = { ...toy };
+
+//Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
+const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsLis2 = [54, 87, 99, 65, 32];
+const pointsList3 = [...pointsList, ...pointsLis2];
+
+//  Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos con spread operators.
+const toy = { name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor' };
+const toyUpdate = { lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk'] }
+const toyCopy = { ...toy, ...toyUpdate };
+
+// Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const colorsCopy = [...colors];
+colorsCopy.splice(2, 1);
+
+// Ejercicio 4
+// Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
+const users = [
+    { id: 1, name: 'Abel' },
+    { id: 2, name: 'Julia' },
+    { id: 3, name: 'Pedro' },
+    { id: 4, name: 'Amanda' }
+];
+
+
+// Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+const users = [
+    { id: 1, name: 'Abel' },
+    { id: 2, name: 'Julia' },
+    { id: 3, name: 'Pedro' },
+    { id: 4, name: 'Amanda' }
+];
+ 
+// Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)'cuando el valor de la propiedad isVisited = true.
+const cities = [
+    { isVisited: true, name: 'Tokyo' },
+    { isVisited: false, name: 'Madagascar' },
+    { isVisited: true, name: 'Amsterdam' },
+    { isVisited: false, name: 'Seul' }
+];
